2b5f8b8e0b8bb9a025875714cd9cb6be4a107ee1
Mamat1411 <muhammadmujahid14@gmail.com>
2022-01-05 09:51:39 +0700

    Function Declaration Object Review

--- script.js
+++ script.js
@@ -1,13 +1,33 @@
 //Ways to create an object
 //1. Literally created object
-let student = {
-    name : "Muhammad",
-    energy : 10,
-    eat : function(portions) {
-        this.energy =+ portions;
+// let student = {
+//     name : "Muhammad",
+//     energy : 10,
+//     eat : function(portions) {
+//         this.energy =+ portions;
+//         console.log(`Enjoy the food ${this.name}!`);
+//     }
+// }
+//2. Function Declaration object
+function students(name, energy) {
+    let student = {};
+    student.name = name;
+    student.energy = energy;
+
+    student.eat = function(portions) {
+        this.energy += portions;
         console.log(`Enjoy the food ${this.name}!`);
     }
+
+    student.game = function(hours){
+        this.energy -= hours;
+        console.log(`Happy Gaming ${this.name}!`);
+    }
+
+    return student;
 }
-//2. Function Declaration object
+
+let student1 = students("Muhammad", 10);
+let student2 = students("Mamat", 10);
 //3. Constructor Function object
 //4. Object.create()
