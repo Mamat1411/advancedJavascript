543f7d344a9ff6760e61842416b120c698359d13
Mamat1411 <muhammadmujahid14@gmail.com>
2022-01-05 09:56:42 +0700

    Constructor Created Object

--- script.js
+++ script.js
@@ -9,25 +9,41 @@
 //     }
 // }
 //2. Function Declaration object
-function students(name, energy) {
-    let student = {};
-    student.name = name;
-    student.energy = energy;
+// function students(name, energy) {
+//     let student = {};
+//     student.name = name;
+//     student.energy = energy;
 
-    student.eat = function(portions) {
+//     student.eat = function(portions) {
+//         this.energy += portions;
+//         console.log(`Enjoy the food ${this.name}!`);
+//     }
+
+//     student.game = function(hours){
+//         this.energy -= hours;
+//         console.log(`Happy Gaming ${this.name}!`);
+//     }
+
+//     return student;
+// }
+
+// let student1 = students("Muhammad", 10);
+// let student2 = students("Mamat", 10);
+//3. Constructor Created Object(new keyword)
+function Students(name, energy) {
+    this.name = name;
+    this.energy = energy;
+
+    this.eat = function(portions) {
         this.energy += portions;
         console.log(`Enjoy the food ${this.name}!`);
     }
 
-    student.game = function(hours){
+    this.game = function(hours){
         this.energy -= hours;
         console.log(`Happy Gaming ${this.name}!`);
     }
-
-    return student;
 }
 
-let student1 = students("Muhammad", 10);
-let student2 = students("Mamat", 10);
-//3. Constructor Function object
+let student1 = new Students("Muhammad", 10);
 //4. Object.create()
