3b727e4e65eb4fdc1da6c5aebff79852fbebd16e
Mamat1411 <muhammadmujahid14@gmail.com>
2022-01-17 11:45:53 +0700

    Arrow Function Implementation on HTML and CSS

--- script.js
+++ script.js
@@ -168,3 +168,18 @@
 //     // }, 3000);
 // }
 // const mamat = new students();
+
+const box = document.querySelector('.box');
+box.addEventListener('click', function () {
+    let one = 'size';
+    let two = 'caption';
+
+    if (this.classList.contains(one)) {
+        [one, two] = [two, one];
+    }
+
+    this.classList.toggle(one);
+    setTimeout(() => {
+        this.classList.toggle(two);
+    }, 600);
+})
