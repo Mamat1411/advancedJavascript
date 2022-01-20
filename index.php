c1a595ce959d444d040f89266a3fe8f86e204c64
Mamat1411 <muhammadmujahid14@gmail.com>
2022-01-20 10:39:02 +0700

    Generating HTML Template for Exercising Filter, Map, Reduce Using Higher Order Function Concept

--- index.php
+++ index.php
@@ -8,7 +8,30 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <!-- <div class="box"></div> -->
+    <h3>My Videos</h3>
+    <ul>
+        <li data-duration="15:27">Podomoro Technique</li>
+        <li data-duration="11:18">ADVANCED JAVASCRIPT | Higher Order Functions</li>
+        <li data-duration="21:40">ADVANCED JAVASCRIPT | This in Arrow Functions</li>
+        <li data-duration="19:38">Scam Sites</li>
+        <li data-duration="12:10">ADVANCED JAVASCRIPT | Arrow Functions</li>
+        <li data-duration="20:43">ADVANCED JAVASCRIPT | Closures</li>
+        <li data-duration="14:30">#ASKMRDIKA EP005</li>
+        <li data-duration="26:38">ADVANCED JAVASCRIPT | Execution Context</li>
+        <li data-duration="17:33">ADVANCED JAVASCRIPT | Prototype</li>
+        <li data-duration="10:39">ADVANCED JAVASCRIPT | Object.Create</li>
+        <li data-duration="17:31">ADVANCED JAVASCRIPT | Object (Revisited)</li>
+        <li data-duration="14:25">5 Questioning Tips when Error Occurs</li>
+    </ul>
+
+    <h3>My Playlists</h3>
+    <ol>
+        <li>
+            <h4>Advanced Javascript</h4>
+            <p>Total Videos: <span class="total-videos"></span></p>
+            <p>Total Duration: <span class="total-duration"></span></p>
+        </li>
+    </ol>
 <script src="script.js"></script>
 </body>
 </html>
