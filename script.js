50174dfac970c36e5b53e14da44feafe7bca4f7c
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-09 12:19:12 +0700

    Fetch Exercise with OMDB API

--- script.js
+++ script.js
@@ -740,6 +740,7 @@
 // console.log('finish');
 
 //Asynchronous Callback Exercise with OMDB API
+//Using jQuery Ajax
 // $('.search-button').on('click', function (){
 //     $.ajax({
 //         url: 'http://www.omdbapi.com/?apikey=bc20012f&s=' + $('.input-keyword').val(),
@@ -771,41 +772,71 @@
 //     });
 // });
 
-// function showCards(m) {
-//     return `
-//             <div class="col-md-3 my-3">
-//                 <div class="card">
-//                     <img src="${m.Poster}" class="card-img-top">
-//                     <div class="card-body">
-//                         <h5 class="card-title">${m.Title}</h5>
-//                         <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-//                         <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Read More</a>
-//                     </div>
-//                 </div>
-//             </div>
-//         `;
-// }
+//Using Fetch
+const searchButton = document.querySelector('.search-button');
+searchButton.addEventListener('click', function () {
+    const inputKeyword = document.querySelector('.input-keyword');
+    fetch('http://www.omdbapi.com/?apikey=bc20012f&s=' + inputKeyword.value)
+        .then(response => response.json())
+        .then(response => {
+            const movies = response.Search;
+            let cards = '';
+            movies.forEach(m => cards += showCards(m));
+            const movieContainer = document.querySelector('.movie-container');
+            movieContainer.innerHTML = cards;
 
-// function showDetails(m) {
-//     return `
-//             <div class="container-fluid">
-//                 <div class="row">
-//                     <div class="col-md-3">
-//                         <img src="${m.Poster}" class="img-fluid">
-//                     </div>
-//                     <div class="col-md">
-//                         <ul class="list-group">
-//                         <li class="list-group-item"><h3><strong>${m.Title}</strong></h3></li>
-//                         <li class="list-group-item"><strong>Director: </strong>${m.Director}</li>
-//                         <li class="list-group-item"><strong>Actors: </strong>${m.Actors}</li>
-//                         <li class="list-group-item"><strong>Writer: </strong>${m.Writer}</li>
-//                         <li class="list-group-item"><strong>Plot: </strong><br>${m.Plot}</li>
-//                         </ul>
-//                     </div>
-//                 </div>
-//             </div>
-//             `;
-// }
+            //when read more button is pushed
+            const readMore = document.querySelectorAll('.modal-detail-button');
+            readMore.forEach(r => {
+                r.addEventListener('click', function () {
+                   const imdbid = this.dataset.imdbid;
+                   fetch('http://www.omdbapi.com/?apikey=bc20012f&i=' + imdbid)
+                    .then(response => response.json())
+                    .then(response => {
+                        const movieDetail = showDetails(response);
+                        const modalBody = document.querySelector('.modal-body');
+                        modalBody.innerHTML = movieDetail;
+                    });
+                });
+            });
+        });
+});
+
+function showCards(m) {
+    return `
+            <div class="col-md-3 my-3">
+                <div class="card">
+                    <img src="${m.Poster}" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">${m.Title}</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+                        <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Read More</a>
+                    </div>
+                </div>
+            </div>
+        `;
+}
+
+function showDetails(m) {
+    return `
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="${m.Poster}" class="img-fluid">
+                    </div>
+                    <div class="col-md">
+                        <ul class="list-group">
+                        <li class="list-group-item"><h3><strong>${m.Title}</strong></h3></li>
+                        <li class="list-group-item"><strong>Director: </strong>${m.Director}</li>
+                        <li class="list-group-item"><strong>Actors: </strong>${m.Actors}</li>
+                        <li class="list-group-item"><strong>Writer: </strong>${m.Writer}</li>
+                        <li class="list-group-item"><strong>Plot: </strong><br>${m.Plot}</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            `;
+}
 
 //jQuery Ajax
 // $.ajax({
@@ -876,32 +907,32 @@
 
 //Promise.all()
 // It is used to run many promises simultaneously
-const movies = new Promise(resolve => {
-    setTimeout(() => {
-       resolve([{
-            title: 'Avengers',
-            director: 'Muhammad',
-            actors: 'Sari, Mamat'
-       }]);
-    }, 1000);
-});
+// const movies = new Promise(resolve => {
+//     setTimeout(() => {
+//        resolve([{
+//             title: 'Avengers',
+//             director: 'Muhammad',
+//             actors: 'Sari, Mamat'
+//        }]);
+//     }, 1000);
+// });
 
-const weather = new Promise(resolve => {
-    setTimeout(() => {
-        resolve([{
-            city: 'Yogyakarta',
-            temperature: 26,
-            condition: 'Cloudy'
-        }]);
-    }, 500);
-});
+// const weather = new Promise(resolve => {
+//     setTimeout(() => {
+//         resolve([{
+//             city: 'Yogyakarta',
+//             temperature: 26,
+//             condition: 'Cloudy'
+//         }]);
+//     }, 500);
+// });
 
-// movies.then(response => console.log(response));
-// weather.then(response => console.log(response));
-Promise.all([movies, weather])
-// .then(response => console.log(response));
-.then(response => {
-    const [movies, weather] = response;
-    console.log(movies);
-    console.log(weather);
-});
+// // movies.then(response => console.log(response));
+// // weather.then(response => console.log(response));
+// Promise.all([movies, weather])
+// // .then(response => console.log(response));
+// .then(response => {
+//     const [movies, weather] = response;
+//     console.log(movies);
+//     console.log(weather);
+// });
