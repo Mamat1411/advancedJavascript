2497ac42b0bd60538ba65ae2ab368241a4d3e16b
Mamat1411 <muhammadmujahid14@gmail.com>
2022-03-10 15:17:47 +0700

    Async Await Basic Example

--- script.js
+++ script.js
@@ -803,82 +803,82 @@
 // });
 
 //Fetch Refactoring (Async Await)
-const searchButton = document.querySelector('.search-button');
-searchButton.addEventListener('click', async function () {
-    const inputKeyword = document.querySelector('.input-keyword');
-    const movies = await getMovies(inputKeyword.value);
-    updateUI(movies);
-});
+// const searchButton = document.querySelector('.search-button');
+// searchButton.addEventListener('click', async function () {
+//     const inputKeyword = document.querySelector('.input-keyword');
+//     const movies = await getMovies(inputKeyword.value);
+//     updateUI(movies);
+// });
 
-//Event Binding for Read More Button
-document.addEventListener('click', async function (e) {
-    if (e.target.classList.contains('modal-detail-button')) {
-        const imdbid = e.target.dataset.imdbid;
-        const movieDetail = await getMovieDetails(imdbid);
-        updateUIDetail(movieDetail);
-    }
-});
+// //Event Binding for Read More Button
+// document.addEventListener('click', async function (e) {
+//     if (e.target.classList.contains('modal-detail-button')) {
+//         const imdbid = e.target.dataset.imdbid;
+//         const movieDetail = await getMovieDetails(imdbid);
+//         updateUIDetail(movieDetail);
+//     }
+// });
 
-function getMovieDetails(imdbid) {
-    return fetch('http://www.omdbapi.com/?apikey=bc20012f&i=' + imdbid)
-            .then(response => response.json())
-            .then(response => response);
-}
+// function getMovieDetails(imdbid) {
+//     return fetch('http://www.omdbapi.com/?apikey=bc20012f&i=' + imdbid)
+//             .then(response => response.json())
+//             .then(response => response);
+// }
 
-function updateUIDetail(movie) {
-    const movieDetail = showDetails(movie);
-    const modalBody = document.querySelector('.modal-body');
-    modalBody.innerHTML = movieDetail;
-}
+// function updateUIDetail(movie) {
+//     const movieDetail = showDetails(movie);
+//     const modalBody = document.querySelector('.modal-body');
+//     modalBody.innerHTML = movieDetail;
+// }
 
-function getMovies(keyword) {
-    return fetch('http://www.omdbapi.com/?apikey=bc20012f&s=' + keyword)
-            .then(response => response.json())
-            .then(response => response.Search);
-}
+// function getMovies(keyword) {
+//     return fetch('http://www.omdbapi.com/?apikey=bc20012f&s=' + keyword)
+//             .then(response => response.json())
+//             .then(response => response.Search);
+// }
 
-function updateUI(movies) {
-    let cards = '';
-    movies.forEach(m => cards += showCards(m));
-    const movieContainer = document.querySelector('.movie-container');
-    movieContainer.innerHTML = cards;
-}
+// function updateUI(movies) {
+//     let cards = '';
+//     movies.forEach(m => cards += showCards(m));
+//     const movieContainer = document.querySelector('.movie-container');
+//     movieContainer.innerHTML = cards;
+// }
 
-function showCards(m) {
-    return `
-            <div class="col-md-3 my-3">
-                <div class="card">
-                    <img src="${m.Poster}" class="card-img-top">
-                    <div class="card-body">
-                        <h5 class="card-title">${m.Title}</h5>
-                        <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
-                        <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Read More</a>
-                    </div>
-                </div>
-            </div>
-        `;
-}
+// function showCards(m) {
+//     return `
+//             <div class="col-md-3 my-3">
+//                 <div class="card">
+//                     <img src="${m.Poster}" class="card-img-top">
+//                     <div class="card-body">
+//                         <h5 class="card-title">${m.Title}</h5>
+//                         <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+//                         <a href="#" class="btn btn-primary modal-detail-button" data-bs-toggle="modal" data-bs-target="#movieDetailModal" data-imdbid="${m.imdbID}">Read More</a>
+//                     </div>
+//                 </div>
+//             </div>
+//         `;
+// }
 
-function showDetails(m) {
-    return `
-            <div class="container-fluid">
-                <div class="row">
-                    <div class="col-md-3">
-                        <img src="${m.Poster}" class="img-fluid">
-                    </div>
-                    <div class="col-md">
-                        <ul class="list-group">
-                        <li class="list-group-item"><h3><strong>${m.Title}</strong></h3></li>
-                        <li class="list-group-item"><strong>Director: </strong>${m.Director}</li>
-                        <li class="list-group-item"><strong>Actors: </strong>${m.Actors}</li>
-                        <li class="list-group-item"><strong>Writer: </strong>${m.Writer}</li>
-                        <li class="list-group-item"><strong>Plot: </strong><br>${m.Plot}</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            `;
-}
+// function showDetails(m) {
+//     return `
+//             <div class="container-fluid">
+//                 <div class="row">
+//                     <div class="col-md-3">
+//                         <img src="${m.Poster}" class="img-fluid">
+//                     </div>
+//                     <div class="col-md">
+//                         <ul class="list-group">
+//                         <li class="list-group-item"><h3><strong>${m.Title}</strong></h3></li>
+//                         <li class="list-group-item"><strong>Director: </strong>${m.Director}</li>
+//                         <li class="list-group-item"><strong>Actors: </strong>${m.Actors}</li>
+//                         <li class="list-group-item"><strong>Writer: </strong>${m.Writer}</li>
+//                         <li class="list-group-item"><strong>Plot: </strong><br>${m.Plot}</li>
+//                         </ul>
+//                     </div>
+//                 </div>
+//             </div>
+//             `;
+// }
 
 //jQuery Ajax
 // $.ajax({
@@ -978,3 +978,40 @@ function showDetails(m) {
 //     console.log(movies);
 //     console.log(weather);
 // });
+
+//Async Await
+// const trying = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve('Finished');
+//     }, 2000);
+// });
+// trying
+//     .then(() => console.log(trying));
+
+function promiseTrying() {
+    return new Promise((resolve, reject) => {
+        const pendingTime = 5000;
+        if (pendingTime < 5000) {
+            setTimeout(() => {
+                resolve('Finished');
+            }, pendingTime);
+        } else {
+            reject('Timed Out');
+        }
+    });
+}
+
+// const test = promiseTrying();
+// test
+//     .then((test) => console.log(test))
+//     .catch((test) => console.log(test));
+
+async function asyncTry() {
+    try {
+        const test = await promiseTrying();
+        console.log(test);
+    } catch (error) {
+        console.error(error);
+    }
+}
+asyncTry();
